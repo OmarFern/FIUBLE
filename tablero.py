4eff5e97cb27007ca9883fd6af66b8a0549e4e89
CrissRM <criss.rol.10@gmail.com>
2022-05-08 14:54:18 -0300

    CrissRM un poco más modularizado

--- tablero.py
+++ tablero.py
@@ -4,6 +4,8 @@ from initialSetting.conditions import *
 from validators.len_palabra import validar_longitud_palabra
 from validators.ganador import valida_ganador
 from generadorLista.correcta import palabra_correcta
+from validators.analizar_palabra import analizar_palabra
+from validators.gana_juego import gana_juego
 
 tablero = tablero_initial_setting()
 
@@ -12,26 +14,13 @@ while (not es_ganador) and (contador_credito< 5):
   palabra = validar_longitud_palabra()
   if valida_ganador(palabra,palabra_lista):
     es_ganador=True 
-    palabra_correcta(palabra)
+    tablero = palabra_correcta(palabra)
   else:
-    lista_2=[]
-    for i in range(len(palabra)):
-      if palabra[i] in palabra_lista[i]:
-        lista_2.append(obtener_color( palabra[i],"Verde" ))
-      elif palabra[i] in palabra_lista:
-        lista_2.append( obtener_color( palabra[i],"Amarillo" ))
-      else:
-        lista_2.append( obtener_color( palabra[i],"Rojo" ))    
-      
-      tablero[contador_credito]=lista_2 
+    lista_2 = analizar_palabra(palabra)
+    tablero[contador_credito]=lista_2 
   
   contador_credito+=1
-  print(contador_credito)
   for i in range(5): 
     print(" ".join(tablero[i]))  
 
-
-if es_ganador :
-    print("ganaste")   
-else:
-    print("la proxima sera la buena ")
+gana_juego(es_ganador)
---
+++ validators/gana_juego.py
@@ -0,0 +1,10 @@
+def gana_juego(es_ganador):
+  return print("Ganaste") if es_ganador else print("Perdiste")
+  
+  
+  
+  
+  # if es_ganador :
+  #   print("ganaste")   
+  # else:
+  #   print("la proxima sera la buena ")
